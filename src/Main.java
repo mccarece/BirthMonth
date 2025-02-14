4f375f6494a699c0c26abcabd540afa5fa200135
chase <mccarece@mail.uc.edu>
2025-02-14 18:08:26 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner console = new Scanner(System.in);
+
+        System.out.print("Enter your birth month (1-12): ");
+        int birthMonth = console.nextInt();
+
+        if (birthMonth > 12 || birthMonth <= 0) {
+            System.out.println("You entered an incorrect month value: " + birthMonth);
+        }
+        else {
+            System.out.println("Your birth month is: " + birthMonth);
+        }
+    }
+}
